28ce5c7f0ba5c083947fe8a4194a3b3e8eb4b1a3
Naoto3615 <supra3615@gmail.com>
2020-05-14 15:53:37 +0900

    items#show
    ・items#show
    items controller : show action
    show.html.haml
    show.scss

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,8 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def show
+    @items=Item.all.includes(:item_images).order('created_at DESC')
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,13 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :area
   belongs_to_active_hash :category
-  belongs_to_active_hash :category1
-  belongs_to_active_hash :category2
+  # belongs_to_active_hash :category1
+  # belongs_to_active_hash :category2
   belongs_to_active_hash :condition
   belongs_to_active_hash :delivery_charge
   belongs_to_active_hash :delivery_day
-  belongs_to_active_hash :size
+  # belongs_to_active_hash :size
   has_many :item_images
+  accepts_nested_attributes_for :item_images
 end
