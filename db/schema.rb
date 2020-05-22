16657a139563cee256e8a7e94bb3ae986bdfb01e
Naoto3615 <supra3615@gmail.com>
2020-05-22 14:56:45 +0900

    payjp_card
    Pre
    完成版

--- db/schema.rb
+++ db/schema.rb
@@ -36,9 +36,9 @@ ActiveRecord::Schema.define(version: 2020_05_21_062159) do
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_cards_on_user_id"
   end
-  
+
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "ancestry"
