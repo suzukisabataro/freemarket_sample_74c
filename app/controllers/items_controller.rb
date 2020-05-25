552c36d85075904d8b2a21a4f8aa8c76781082ea
suzukisabataro <dwdwdwtjagpmdwdwx@gmail.com>
2020-05-25 15:33:28 +0900

    before_action修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, only: [:new, :create, :edit, :update]
-  before_action :set_item_image, except: [:index, :new, :create]
+  before_action :set_item, except: [:index, :new, :create]
   def index
     @items = Item.limit(10).order('created_at DESC')
   end
