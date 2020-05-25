5303fee9aa42dec27d5c4609023349f64225f470
suzukisabataro <dwdwdwtjagpmdwdwx@gmail.com>
2020-05-25 13:32:13 +0900

    [WIP]商品出品機能　ビュー修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  
-  validates :nickname, :birth_yy, :birth_mm, :birth_dd, :first_name, :last_name, :first_name_kana, :last_name_kana, :phone_number,presence: true
   
   has_many :items
   has_one :card
